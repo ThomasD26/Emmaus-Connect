e2ebca046528a16984549c3eb8b390a830fca65f
alexdubrulle <alexdubrullefagnoni26@hotmail.fr>
2023-06-28 14:27:11 +0200

    algo telephone

---
+++ Emmaus-Connect/src/components/PrixTel.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+function calculerValeurTelephone(
+  ram,
+  stockage,
+  etatGlobal,
+  etatEcran,
+  capBatterie,
+  chargeur
+) {
+  // Valeurs attribuées pour chaque élément
+  const valeursRam = {
+    1: 10,
+    2: 20,
+    3: 30,
+    4: 40,
+  };
+  const valeursStockage = {
+    8: 1,
+    16: 2,
+    32: 3.5,
+    64: 5,
+    128: 10,
+    256: 15,
+    512: 20,
+  };
+  const valeursEtatglobal = {
+    mauvais: 0,
+    correct: 10,
+    bon: 15,
+    excellent: 25,
+    neuf: 30,
+  };
+  const valeursEtatecran = {
+    Rayé: 0,
+    correct: 10,
+    bon: 15,
+    excellent: 25,
+  };
+  const valeursCapbatterie = {
+    mauvais: 0,
+    correct: 10,
+    bon: 15,
+    excellent: 25,
+  };
+  const valeursChargeur = {
+    oui: 10,
+    non: -10,
+  };
+
+  // Calcul des valeurs pour la RAM et le stockage
+  const valeurRam = valeursRam[ram];
+  const valeurStockage = valeursStockage[stockage];
+  const valeurEtatGlobal = valeursEtatglobal[etatGlobal];
+  const valeurEtatEcran = valeursEtatecran[etatEcran];
+  const valeurCapbatterie = valeursCapbatterie[capBatterie];
+  const valeurChargeur = valeursChargeur[chargeur];
+
+  // Calcul de la valeur totale
+  let valeurTotale =
+    valeurRam +
+    valeurStockage +
+    valeurEtatGlobal +
+    valeurEtatEcran +
+    valeurCapbatterie +
+    valeurChargeur;
+
+  // Attribution de la lettre correspondante en fonction de la valeur totale
+  let lettreValeurTotale;
+  if (valeurTotale >= 60 && valeurTotale <= 100) {
+    lettreValeurTotale = "C";
+  } else if (valeurTotale > 101 && valeurTotale <= 175) {
+    lettreValeurTotale = "B";
+  } else if (valeurTotale > 176) {
+    lettreValeurTotale = "A";
+  }
+
+  return lettreValeurTotale;
+}
+
+// Exemple d'utilisation de la fonction
+const ram = 4; // en Go
+const stockage = 128; // en Go
+const etatGlobal = "bon";
+const etatEcran = "bon";
+const capBatterie = "excellent";
+const chargeur = "oui";
+
+const valeurTelephone = calculerValeurTelephone(
+  ram,
+  stockage,
+  etatGlobal,
+  etatEcran,
+  capBatterie,
+  chargeur
+);
+console.log("La valeur du téléphone est de " + valeurTelephone);
+
+export default calculerValeurTelephone;
